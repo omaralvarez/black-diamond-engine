19b3cbfded25a1bd05692b32c1474670c6d5c0d9
omar alvarez <xxosurfer3xx@gmail.com>
2012-02-13 19:54:17 +0100

    Added KdTreeAccel(incomplete).

---
+++ black-diamond-engine/kdtreeaccel.cpp
@@ -0,0 +1,9 @@
+//
+//  kdtreeaccel.cpp
+//  black-diamond-engine
+//
+//  Created by Luis Omar Alvarez Mures on 2/13/12.
+//  Copyright (c) 2012 UDC. All rights reserved.
+//
+
+#include <iostream>
---
+++ black-diamond-engine/kdtreeaccel.h
@@ -0,0 +1,14 @@
+//
+//  kdtreeaccel.h
+//  black-diamond-engine
+//
+//  Created by Luis Omar Alvarez Mures on 2/13/12.
+//  Copyright (c) 2012 UDC. All rights reserved.
+//
+
+#ifndef black_diamond_engine_kdtreeaccel_h
+#define black_diamond_engine_kdtreeaccel_h
+
+
+
+#endif
