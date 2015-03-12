6a66ad3c1daa556e4f65f51654c2f05e249ef49f
osurfer3 <omar.alvarez@udc.es>
2015-03-12 11:16:37 +0100

    License added

---
+++ black-diamond-engine/brdfmerl.cpp
@@ -0,0 +1,9 @@
+//
+//  brdfmerl.cpp
+//  black-diamond-engine
+//
+//  Created by Luis Omar Alvarez Mures on 5/24/12.
+//  Copyright (c) 2012 UDC. All rights reserved.
+//
+
+#include <iostream>
---
+++ black-diamond-engine/brdfmerl.h
@@ -0,0 +1,14 @@
+//
+//  brdfmerl.h
+//  black-diamond-engine
+//
+//  Created by Luis Omar Alvarez Mures on 5/24/12.
+//  Copyright (c) 2012 UDC. All rights reserved.
+//
+
+#ifndef black_diamond_engine_brdfmerl_h
+#define black_diamond_engine_brdfmerl_h
+
+
+
+#endif
